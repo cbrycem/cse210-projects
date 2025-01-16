e4cca31bc00e66dd4ed9092ed17f6b257c7cc864
Chris Miller <cbrycem03@gmail.com>
2025-01-16 15:06:05 -0700

    Prep4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> num = new List<int>();
+        int number;
+        int sum = 0;
+        Console.WriteLine("Enter a list of numbers. Enter 0 when finished.");
+        do {
+            Console.Write("Enter Number: ");
+            string userInput = Console.ReadLine();
+            new string(userInput.Where(c => char.IsDigit(c) || c == '.').ToArray());
+            string clean = userInput.Replace("%", "");
+            number = int.Parse(clean);
+            if (number == 0) {
+                break;
+            }
+            num.Add(number);
+        } while (number != 0);
+
+        for (int i = 0; i < num.Count; i++) {
+            sum = sum + num[i];
+        }
+        
+        Console.WriteLine("The sum is: " + sum);
+        
+        float avg = (float)sum / (float)num.Count;
+        Console.WriteLine("The average is: " + avg);
+
+        int max = num.Max();
+        Console.WriteLine("The largest number is: " + max);
+
     }
 }
