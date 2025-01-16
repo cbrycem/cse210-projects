1954070e9607f31b3ad026fd92cfbc4cfd8bb6e7
Chris Miller <cbrycem03@gmail.com>
2025-01-16 08:54:21 -0700

    Prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator= new Random();
+        string response = "a";
+        int guess = 0;
+        do {   
+            int number = randomGenerator.Next(1, 100);
+            Console.WriteLine("Thank you for playing this game. \nI have chosen a number between 1 and 100, and you get to guess it! Start guessing!");
+            while (guess != number) {
+                Console.Write("Guess the Secret Number: ");
+                string userInput = Console.ReadLine();
+                new string(userInput.Where(c => char.IsDigit(c) || c == '.').ToArray());
+                string clean = userInput.Replace("%", "");
+                guess = int.Parse(clean);
+            
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                } else if (guess < number) {
+                    Console.WriteLine("Higher");
+                }
+            }
+            Console.WriteLine("Congratulations, you have guessed the number!");
+            Console.Write("Would you like to play again? (y/n)");
+            response = Console.ReadLine();
+        } while (response == "y");
+        //Console.WriteLine("Hello Prep3 World!");
     }
 }
